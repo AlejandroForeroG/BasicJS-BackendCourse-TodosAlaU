350eac28ff1e6fbef53b05d7f6afd7e665e8542c
Neve <alejandro.forerog@gmail.com>
2023-03-22 11:12:00 -0500

    homework js

---
+++ Untitled-2.js
@@ -0,0 +1,24 @@
+//definir dos variabes, asignarles un valor  y utilizando desiciones imprimir en la consola el que tenga menor valor
+
+let variableA= 3;
+let variableB= 6;
+
+if (variableA>variableB){
+    console.log(`El numero mayor es: ${variableA}`)
+}else{
+    console.log(`El numero mayor es: ${variableB}`)
+}
+
+ // Definir tres variables, asignarles un valor y utilizando decisiones imprimir en la consola la que tenga mayor valor
+ let a = 4;
+ let b = 5;
+ let c = 1;
+
+if(a > b && a>c){
+    console.log(`El numero mayor es ${a}`)
+}else if(b>a && b>c){
+    console.log(`El numero mayor es ${b}`)
+}else if (c>a && c>b){
+    console.log(`El numero mayor es ${c}`)
+
+}
